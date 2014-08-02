e5688d706f6a58c4887cfe74e7033ea0bfecd362
Sedoneh <rcsdailey@gmail.com>
2014-08-02 00:27:27 -0500

    updated main
    main

--- Dailey Bros/main.cpp
+++ Dailey Bros/main.cpp
@@ -20,11 +20,18 @@ using namespace std;
 int main() {
     Warrior Sedon;
     Goblins Mob;
+    
+    cout << "The Goblin mob has" << Mob.get_hp() << " hit points!" << endl;
+    Mob.damage_hp(Sedon.get_warrior_ability());
+    
+    cout << "The Goblin mob now has " << Mob.get_hp() << " hit points!!" << endl;
+    
     for(int i = 0; i<5; i++){
-        Sedon.increase_experience(Mob.reward_experience());
+        Sedon.increase_experience(Mob.get_reward_experience());
     }
     
     cout << "Sedon has " << Sedon.get_experience() << " experience points!!!!!";
+    
     
     
     return 0;
