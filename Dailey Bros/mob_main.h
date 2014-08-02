8bb2b0c61b6b99e234a63a04799ce3cf893417c7
Sedoneh <rcsdailey@gmail.com>
2014-08-01 23:41:11 -0500

    mob_main.h and main includes
    updated some goblin info

--- Dailey Bros/mob_main.h
+++ Dailey Bros/mob_main.h
@@ -7,18 +7,54 @@
 
 #ifndef MOB_MAIN_H
 #define	MOB_MAIN_H
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
 
-#ifdef	__cplusplus
-extern "C" {
-#endif
-// hey there crakka
+/*class MobType
+{
+    private:
+        string mob_name;
+    public:
+        MobType(){
+        }
+};*/
 
-//asdfasdf
-
-
-#ifdef	__cplusplus
-}
-#endif
+/*
+ * The Goblin monster type.  Has initial hp 15, mp 5.
+ *
+ * 
+ * 
+ * 
+ */
+class Goblins
+{
+    private:
+        string name;
+        string description;
+        int hp;
+        int mp;
+        int exp_reward;
+       
+    public:
+        Goblins(){
+            name = "Goblin";
+            description = "The nasty Goblin race is after many things, but most of all\
+                            is after your money!";
+            hp = 15;
+            mp = 5;
+            exp_reward = 1;
+        }
+        
+        /** Begin Experience fuctions **/
+        // award exp to character that defeated goblin
+        int reward_experience(){
+            return exp_reward;
+        }
+        
+        
+};
 
 #endif	/* MOB_MAIN_H */
 
