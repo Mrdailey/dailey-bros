2505adee7e70459ec1f8533868c7befe75716d54
Sedoneh <rcsdailey@gmail.com>
2014-08-01 22:00:47 -0500

    mob_main.h file created
    testing sd upload

---
+++ Dailey Bros/mob_main.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   mob_main.h
+ * Author: rcsda_000
+ *
+ * Created on August 1, 2014, 9:59 PM
+ */
+
+#ifndef MOB_MAIN_H
+#define	MOB_MAIN_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+// hey there crakka
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MOB_MAIN_H */
+
--- Dailey Bros/nbproject/Makefile-variables.mk
+++ Dailey Bros/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=MinGW-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
 CND_ARTIFACT_NAME_Debug=dailey_bros
-CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/dailey_bros
-CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/dailey_bros
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
 CND_PACKAGE_NAME_Debug=daileybros.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/daileybros.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/daileybros.tar
 # Release configuration
-CND_PLATFORM_Release=MinGW-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
 CND_ARTIFACT_NAME_Release=dailey_bros
-CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/dailey_bros
-CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/dailey_bros
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
 CND_PACKAGE_NAME_Release=daileybros.tar
-CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/daileybros.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/daileybros.tar
 #
 # include compiler specific variables
 #
