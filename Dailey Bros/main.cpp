3de555a0f49ffcec61e91c27e17161cc6a6910e0
Michael <sedon98@aol.com>
2014-08-01 21:11:42 -0500

    Added Project to netbeans

---
+++ Dailey Bros/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Michael Ryan
+ *
+ * Created on August 1, 2014, 9:10 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
